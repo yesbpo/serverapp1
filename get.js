64ff8cb4db4461926e27e3363cf1a8a9ad9bfaa1
unknown <philiprockert@gmail.com>
2023-12-29 08:06:24 -0500

    subida al servidor

--- get.js
+++ get.js
@@ -61,29 +61,34 @@ app.all('/w/api/index', async (req, res) => {
     try {
       var data = req.body;
       await processAsync(data);
+      const responseChat = await fetch('https://appcenteryes.appcenteryes.com/db/obtener-chats');
+      const chats = await responseChat.json();
+      if(data.payload.source){
+        const chatlimpio = chats.filter(chat=> chat.idChat2 == data.payload.source);
+        if(chatlimpio.status == 'closed'){
+          const data1 = {
+            // Asigna el valor actual del contador y luego incrementa
+           idChat2:chatlimpio.idChat2 ,
+           resolved: false,
+           status: 'pending',
+           userId: 0,
+         };
+         const response = await fetch('https://appcenteryes.appcenteryes.com/db/crear-chat', {
+           method: 'POST',
+           headers: {
+             'Content-Type': 'application/json',
+           },
+           body: JSON.stringify(data1),
+         });
+         if (!response.ok) {
+           console.log('no exito')       
+         }
+         const responseData = await response.json();
+   console.log('exito')
+         }
           
         
-      const data1 = {
-        // Asigna el valor actual del contador y luego incrementa
-       idChat2:numeroNormalizado ,
-       resolved: false,
-       status: 'pending',
-       userId: 0,
-     };
-     const response = await fetch('https://appcenteryes.appcenteryes.com/db/crear-chat', {
-       method: 'POST',
-       headers: {
-         'Content-Type': 'application/json',
-       },
-       body: JSON.stringify(data1),
-     });
-     if (!response.ok) {
-       console.log('no exito')       
-     }
-     const responseData = await response.json();
-console.log('exito')
-     
-     
+      }
       
       
       
