160b8635a6b2ad506d7b0c5bfc8c923d10d211a3
unknown <philiprockert@gmail.com>
2024-01-03 14:18:08 -0500

    subida al servidor

--- get.js
+++ get.js
@@ -170,6 +170,27 @@ const segundos = fechaActual.toLocaleString('en-US', { second: '2-digit', timeZo
       const chatlimpio = chats.filter(chat=> chat.idChat2 == data.payload.source);
 
       const chatlimpio1 = chats.filter(chat=> chat.status == 'closed')
+      if(chatlimpio.length === 0){
+        const data3 = {
+            
+          idChat2: data.payload.source,
+          resolved: false,
+          status: 'pending',
+          userId: 0,
+        };
+        const response = await fetch('https://appcenteryes.appcenteryes.com/db/crear-chat', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data3),
+        });  
+        if (!response.ok) {
+          console.log('no exito')       
+        }
+        const responseData = await response.json();
+         console.log(responseData)
+      }
       
       if(chatlimpio[0].status == 'closed'&& data.type == 'message'){
         console.log('entra en if')
@@ -197,28 +218,7 @@ const segundos = fechaActual.toLocaleString('en-US', { second: '2-digit', timeZo
          const responseData = await response.json();
           console.log(responseData)
          
-      }else if(chatlimpio.length === 0){
-        const data3 = {
-            
-          idChat2: data.payload.source,
-          resolved: false,
-          status: 'pending',
-          userId: 0,
-        };
-        const response = await fetch('https://appcenteryes.appcenteryes.com/db/crear-chat', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data3),
-        });  
-        if (!response.ok) {
-          console.log('no exito')       
-        }
-        const responseData = await response.json();
-         console.log(responseData)
       }
-      
       const respnse1 = await respnseweb.json();
       console.log(respnse1)
       
